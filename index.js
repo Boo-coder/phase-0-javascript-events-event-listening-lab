226457fb848f8b4394b66c7480897d88a407a157
Boo-coder <Bouna112@yahoo.com>
2024-10-15 15:17:33 -0400

    18HTML

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById("button")
+    input.addEventListener('click', function addEventListener() {
+        alert('I was pushed!');
+    })
 }
+
+document.addEventListener('click', () => alert('clicked!'))
