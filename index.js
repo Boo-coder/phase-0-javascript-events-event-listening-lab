65efd407e9d10d14bb043df65935a1136fd8d2d2
Boo-coder <Bouna112@yahoo.com>
2025-02-17 21:02:52 -0500

    lst

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function addingEventListener() {
+
+function addingEventListener(){
     const input = document.getElementById("button")
-    input.addEventListener('click', function addEventListener() {
-        alert('I was pushed!');
+    input.addEventListener("click", function addEventListener(){
+        alert("I was pushed!")
     })
 }
-
-document.addEventListener('click', () => alert('clicked!'))
+input.addEventListener("click", clickAlert)
